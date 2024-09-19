dd9ca47a85d6c50af9c60007bc9650fb978e4eb6
Alexis Sanchez <aasvdj@gmail.com>
2024-09-19 18:22:02 -0400

    feat: add cors, useGlobalPipes, addBearerAuth

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,47 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Nolatech 360 Evaluation')
     .setDescription('The Nolatech 360 Evaluation API description')
     .setVersion('1.0')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
+  Object.values((document as OpenAPIObject).paths).forEach((path: any) => {
+    Object.values(path).forEach((method: any) => {
+      if (
+        Array.isArray(method.security) &&
+        method.security.includes('public')
+      ) {
+        method.security = [];
+      }
+    });
+  });
+  SwaggerModule.setup('documentation', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
 
   await app.listen(3000);
 }
